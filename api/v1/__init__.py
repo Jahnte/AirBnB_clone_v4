3c91e486b6d94802d3db042ed9b09b3a894a94a0
akiyafaith <akiyafaith@gmail.com>
2023-07-31 20:28:33 +0000

    task 3

---
+++ api/v1/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from flask import Blueprint
+"""a variable with a blueprint"""
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+"""import views module wont be checked for PEP8"""
+import api.v1.views
---
+++ api/v1/views/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""importBlueprint"""
+
+from flask import Blueprint
+"""create a variable with an instance Blueprint"""
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+import api.v1.views.index
