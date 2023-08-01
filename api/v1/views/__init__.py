c891db0361026da62e91604cc87c369dcb0f3919
akiyafaith <akiyafaith@gmail.com>
2023-08-01 23:13:12 +0000

    updated

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,6 +9,3 @@ import api.v1.views.index
 import api.v1.views.states
 import api.v1.views.cities
 import api.v1.views.users
-import api.v1.views.places_reviews
-import api.v1.views.amenities
-import api.v1.views.places
